edd28a1a336a1e36a6876eddbcf9921c4f399fc6
C4ners <121325451+C4ners@users.noreply.github.com>
2025-07-21 01:22:54 -0300

    Add handlebars template and connect to the db

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express')
 const app = express()
+const handlebars = require('express-handlebars')
+
+//Conection with the database
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('backend_project1', 'root', '123',{
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+//Config
+    //Template Engine
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
 
 app.listen(7777, function(){
     console.log('server is online')
